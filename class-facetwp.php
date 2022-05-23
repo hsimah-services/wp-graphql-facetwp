7db7c5787c7be9e7f28759019d51a5bb761de985
Matteo De Micheli <matteodem@protonmail.com>
2022-05-23 13:03:15 +0200

    Add filter for facet connection

--- class-facetwp.php
+++ class-facetwp.php
@@ -196,7 +196,7 @@ final class WPGraphQL_FacetWP
         $field = $config['field'];
         $plural = $config['plural'];
 
-        register_graphql_connection([
+        $default_facet_connection_config = [
             'fromType'          => $field,
             'toType'            => $singular,
             'fromFieldName'     => lcfirst($plural),
@@ -210,7 +210,15 @@ final class WPGraphQL_FacetWP
                     ->set_query_arg('post__in', $source['results'])
                     ->get_connection();
             },
-        ]);
+        ];
+
+        $graphql_connection_config = apply_filters(
+            'facetwp_graphql_facet_connection_config',
+            $default_facet_connection_config,
+            $config
+        );
+
+        register_graphql_connection($graphql_connection_config);
     }
 
     /**
