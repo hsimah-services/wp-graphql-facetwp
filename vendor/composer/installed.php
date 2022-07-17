3a3b3e4f8ba1e467a70a78905517f753fe664801
David Levine <justlevine@gmail.com>
2022-07-17 08:22:04 +0000

    chore: regenerate autoloader

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '6ddb8d29f792bd9268275689c907f5c1060555ed',
+        'reference' => '28be5cd429f94a0430b8a9650796aa949b5da838',
         'name' => 'hsimah-services/wp-graphql-facetwp',
         'dev' => false,
     ),
     'versions' => array(
         'hsimah-services/wp-graphql-facetwp' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '6ddb8d29f792bd9268275689c907f5c1060555ed',
+            'reference' => '28be5cd429f94a0430b8a9650796aa949b5da838',
             'dev_requirement' => false,
         ),
     ),
