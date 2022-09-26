d71dd9f962310739c1bf8c0a1219d2b9d577d4cf
Dovid Levine <david@axepress.dev>
2022-09-26 16:06:19 +0300

    tests: configure codeception (#37)
    * chore: configure codeception
    
    * ci: add GH workflow for codeception

---
+++ tests/acceptance/ActivationCest.php
@@ -0,0 +1,24 @@
+<?php
+
+class ActivationCest {
+	// tests
+	public function testActivation( AcceptanceTesterActions $I ) {
+		$pluginSlug = 'wpgraphql-for-facetwp';
+
+		$I->wantTo( 'activate and deactivate the plugin correctly' );
+
+		$I->loginAsAdmin();
+		$I->amOnPluginsPage();
+		$I->seePluginActivated( $pluginSlug );
+		$I->deactivatePlugin( $pluginSlug );
+
+		$I->loginAsAdmin();
+		$I->amOnPluginsPage();
+		$I->seePluginDeactivated( $pluginSlug );
+		$I->activatePlugin( $pluginSlug );
+
+		$I->loginAsAdmin();
+		$I->amOnPluginsPage();
+		$I->seePluginActivated( $pluginSlug );
+	}
+}
