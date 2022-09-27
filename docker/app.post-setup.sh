d701e4f95ec2102ed0b7c4c4436a7646aad35390
David Levine <david@axepress.dev>
2022-09-27 23:56:03 +0000

    test: fix docker plugin install path

--- docker/app.post-setup.sh
+++ docker/app.post-setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
-
 install_facetwp() {
 	if [ ! -d $WP_CORE_DIR/wp-content/plugins/facetwp ]; then
 		echo "Cloning FacetWP: https://${GIT_TOKEN}@${FACET_REPO}"
-		git clone -b master --single-branch https://${GIT_TOKEN}@${FACET_REPO} $WP_CORE_DIR/wp-content/plugins/facetwp
+		git clone -b master --single-branch https://${GIT_TOKEN}@${FACET_REPO} $PLUGINS_DIR/facetwp
 	fi
 	echo "Activating FacetWP"
 	wp plugin activate facetwp --allow-root
