a47a807dfbd814d1288c9464004df56c60f04bf5
David Levine <david@axepress.dev>
2023-01-12 13:19:04 +0000

    chore: update stub files and deps

---
+++ phpstan/class-facetwp.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * FacetWP
+ *
+ * @property FacetWP_API_Fetch $api
+ * @property FacetWP_Helper $helper
+ * @property FacetWP_Renderer $facet
+ * @property FacetWP_Settings $settings
+ * @property FacetWP_Diff $diff
+ * @property FacetWP_Indexer $indexer
+ * @property FacetWP_Display $display
+ * @property FacetWP_Builder $builder
+ * @property FacetWP_Requext $requext
+ * @property FacetWP_Ajax $ajax
+ */
+class FacetWP {}
+
+/**
+ * Returns the PHPStan instance.
+ *
+ * @return \FacetWP
+ */
+function FWP() {}
