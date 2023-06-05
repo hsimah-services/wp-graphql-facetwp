1fc5953a7a4deb19a938fd0cb7e487edcbff54d7
David Levine <david@axepress.dev>
2023-06-05 11:01:12 +0000

    chore: update deps with final versions

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'hsimah-services/wp-graphql-facetwp',
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
-        'reference' => '6c9dbe32ae2eee9e8f581f19472120e1d455108c',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3871200b8831bc7aed113f5f5d6201087a46f72e',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'axepress/wp-graphql-plugin-boilerplate' => array(
-            'pretty_version' => 'dev-dev/remove-type_registry-property',
-            'version' => 'dev-dev/remove-type_registry-property',
-            'reference' => '689d10c9f20c046e255fc11ca53fc201e6fe47b7',
+            'pretty_version' => '0.0.9',
+            'version' => '0.0.9.0',
+            'reference' => '5062b5b8896d14b8ee566b92fffa4d049e7abd34',
             'type' => 'library',
             'install_path' => __DIR__ . '/../axepress/wp-graphql-plugin-boilerplate',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'hsimah-services/wp-graphql-facetwp' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
-            'reference' => '6c9dbe32ae2eee9e8f581f19472120e1d455108c',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3871200b8831bc7aed113f5f5d6201087a46f72e',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
